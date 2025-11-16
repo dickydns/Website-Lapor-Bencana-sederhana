a43b3a5b331406b60526273b042af4d67a08ef3a
Dicky Perdian <dickydraknes@gmail.com>
2025-11-16 17:51:14 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'prisma']
-  },
-  // Tambahkan ini untuk Netlify
-  output: 'export', // Kalau pure static
-  // ATAU kalau pakai API routes:
-  // Jangan pakai output: 'export'
+  }
 }
 
 export default nextConfig
