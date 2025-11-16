422cc63f428fd1bb1fae2fe68356c3dda3c60895
lusymaharani <lusymaharani@gmail.com>
2025-11-16 20:11:22 +0700

    first test

--- cypress/e2e/addnewreport.cy.ts
+++ cypress/e2e/addnewreport.cy.ts
@@ -5,11 +5,16 @@ import { faker } from '@faker-js/faker';
 describe('User Report', () => {
   it('Add new report', () => {
     newReport.visit();
+    cy.get('#nama').type('bianca');
+    cy.get('#telepon').type('08654323451');
+    cy.get('#email').type('bianca@mailme.com');
+    cy.get('#laporanForm input[placeholder="--"]').click();
+    cy.get('#laporanForm input[placeholder="--"]').type('sampah sembarangan mengakibatkan banjir');
+    cy.get('#kategori').select('4');
+    cy.get('#lokasi').type('jln. mawar 125');
+    cy.get('#deskripsi').type('banyak tumpukan sampah di sisi sungai mngakibatkan bau tidak sedap saat banjir');
+    cy.get('#laporanForm button.text-white').click();
+    cy.get('#laporanForm div.alert').click();
   })
   
 })
-
-it('newreport', function() {
-  cy.visit('http://localhost:3000/')
-  
-});
