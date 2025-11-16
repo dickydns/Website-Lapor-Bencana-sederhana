0df6c5cfb198246268d5ac4bdac5f67a6bc76dc0
Dicky Perdian <dickydraknes@gmail.com>
2025-11-16 17:23:34 +0700

    json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || []
+      config.externals.push({
+        '@prisma/client': 'commonjs @prisma/client',
+        'prisma': 'commonjs prisma'
+      })
+    }
+    return config
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
+  }
+}
 
-export default nextConfig;
+export default nextConfig
